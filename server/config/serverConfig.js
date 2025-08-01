e58d0b9638e3dd437c76ae5034fdc239b800758f
Luqman <123114107+LuqmanAtcha@users.noreply.github.com>
2025-08-01 10:16:20 -0400

    Update serverConfig.js

--- server/config/serverConfig.js
+++ server/config/serverConfig.js
@@ -10,7 +10,7 @@ export const setupServer = () => {
   const corsOptions = {
     origin: "https://deployed-game-show.vercel.app",
     methods: ["GET", "POST"],
-    credentials: true, // <- optional but useful
+    credentials: true,
   };
   const io = new Server(server, {
     cors: corsOptions,
@@ -18,7 +18,7 @@ export const setupServer = () => {
 
   // Middleware
   app.use(helmet());
-  app.use(cors());
+  app.use(cors(corsOptions)); // ✅ use same CORS options as Socket.IO
   app.use(json());
 
   return { app, server, io };
